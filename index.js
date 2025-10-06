ab69ac653ee44101bd421c994ea2e26505da30bb
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-06 22:15:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 app.get('/', (req, res) => res.send('Backend läuft!'));
 app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
+const cors = require('cors');
+app.use(cors()); // erlaube alle Domains
