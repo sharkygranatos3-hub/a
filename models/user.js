2b00102ec3d1e142848aff2e4a4666e5e691273f
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-11 11:17:43 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  rank: { type: String, default: "Officer" },
+  permissions: { type: Object, default: {} },
+  active: { type: Boolean, default: true } // für Sperrung
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
 
