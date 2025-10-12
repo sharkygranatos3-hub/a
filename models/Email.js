9c852d75811d3f71d386d7d4526dd7ce19942ee1
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-12 16:56:07 +0200

    Update Email.js

--- models/Email.js
+++ models/Email.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
 const emailSchema = new mongoose.Schema({
-  from: { type: String, required: true },
-  to: { type: String, required: true },
+  from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   subject: { type: String, required: true },
   body: { type: String, required: true },
-  attachment: { type: String, default: null }, // Datei-URL
-  date: { type: Date, default: Date.now },
+  images: { type: [String], default: [] }, // Base64 oder Pfad
   read: { type: Boolean, default: false },
-});
+}, { timestamps: true });
 
 export default mongoose.model("Email", emailSchema);
