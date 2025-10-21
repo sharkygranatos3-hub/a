7d20b1f5d6ab2fe7263e216b05a4cbdc859569c9
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-21 12:13:33 +0200

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,4 +1,3 @@
-// models/Employee.js
 import mongoose from "mongoose";
 
 const EmployeeSchema = new mongoose.Schema({
@@ -6,11 +5,10 @@ const EmployeeSchema = new mongoose.Schema({
   lastname: String,
   rank: String,
   username: String,
-  password: String, // hashed mit bcrypt
+  password: String, // Passwort als Hash
   securityLevel: Number,
   trainings: [{ name: String, done: Boolean, date: Date }],
   active: { type: Boolean, default: true }
-});
+}, { collection: "users" }); // 🔹 Force Collection "users"
 
-export default mongoose.model("users", EmployeeSchema);
-
+export default mongoose.models.Employee || mongoose.model("Employee", EmployeeSchema);
