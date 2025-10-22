66620db2ee43a188d4172e93026e586264701f94
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-22 09:54:31 +0200

    Update Training.js

--- models/Training.js
+++ models/Training.js
@@ -1,35 +1,15 @@
 import mongoose from "mongoose";
 
-const moduleSchema = new mongoose.Schema({
-  titel: { type: String, required: true },
-  inhalt: { type: String, default: "" }, // HTML/Text
-  typ: { type: String, enum: ["text", "video", "pdf"], default: "text" },
-  videoUrl: { type: String },
-  pdfUrl: { type: String },
-});
+const TrainingSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  trainer: String,
+  location: String,
+  datetime: Date,
+  maxParticipants: Number,
+  targetGroup: String,
+  requiredRank: String,
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "Employee" }]
+}, { timestamps: true });
 
-const trainingSchema = new mongoose.Schema(
-  {
-    titel: { type: String, required: true },
-    beschreibung: { type: String, required: true },
-    trainer: { type: String, required: true },
-    ort: { type: String },
-    datum: { type: String },
-    uhrzeit: { type: String },
-    maxTeilnehmer: { type: Number, default: 10 },
-    zielgruppe: { type: String, default: "Alle Beamten" },
-    erforderlicherRang: { type: String, default: "Officer" },
-    teilnehmer: [
-      {
-        mitarbeiterId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
-        name: String,
-        bestanden: { type: Boolean, default: false },
-      },
-    ],
-    module: [moduleSchema],
-    erstelltVon: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
-  },
-  { timestamps: true }
-);
-
-export default mongoose.model("Training", trainingSchema);
+export default mongoose.model("Training", TrainingSchema);
