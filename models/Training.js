e37276c2319123c828eeefd3e81b73b9b1e1dc3b
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-22 18:06:22 +0200

    Update Training.js

--- models/Training.js
+++ models/Training.js
@@ -1,15 +1,33 @@
 import mongoose from "mongoose";
 
-const TrainingSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  trainer: String,
-  location: String,
-  datetime: Date,
-  maxParticipants: Number,
-  targetGroup: String,
-  requiredRank: String,
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "Employee" }]
-}, { timestamps: true });
+const moduleSchema = new mongoose.Schema({
+  titel: { type: String, required: true },
+  inhalt: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
 
-export default mongoose.model("Training", TrainingSchema);
+const trainingSchema = new mongoose.Schema(
+  {
+    titel: { type: String, required: true },
+    beschreibung: { type: String },
+    trainer: { type: String },
+    ort: { type: String },
+    zeitpunkt: { type: Date },
+    maxTeilnehmer: { type: Number },
+    zielgruppe: { type: String },
+    erforderlicherRang: { type: String },
+    teilnehmer: [
+      {
+        id: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
+        name: String,
+        bestanden: { type: Boolean, default: false }
+      }
+    ],
+    module: [moduleSchema],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
+    createdAt: { type: Date, default: Date.now }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Training", trainingSchema);
