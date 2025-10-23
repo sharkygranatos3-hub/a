37afed6570a69ac717f5ad7f1d641acc1c84a767
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-23 21:24:12 +0200

    Update wiki.js

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 import express from "express";
 import WikiEntry from "../models/wikiEntry.js";
-import { verifyToken, requireRole } from "../middleware/auth.js";
+import verifyToken, { requireRole } from "../middleware/auth.js";
 
 const router = express.Router();
 
