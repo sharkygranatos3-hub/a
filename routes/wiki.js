9ca455fb68f298469206a5a51a485394f7b921a2
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-23 18:17:49 +0200

    Update wiki.js

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 import express from "express";
 import WikiEntry from "../models/wikiEntry.js";
-import { authMiddleware } from "../middleware/auth.js";
+import authMiddleware from "../middleware/auth.js"; // ✅ kein { } mehr
 
 const router = express.Router();
 
