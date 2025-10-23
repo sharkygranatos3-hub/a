ce5248d451ac61a01a2283f427135a47523ff6ea
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-23 20:17:53 +0200

    Update wikiEntry.js

--- models/wikiEntry.js
+++ models/wikiEntry.js
@@ -1,10 +1,10 @@
+// models/WikiEntry.js
 import mongoose from "mongoose";
 
 const wikiEntrySchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  authorName: { type: String },
-  authorRank: { type: String },
+  author: { type: String, required: true }, // z. B. "John Doe"
 }, { timestamps: true });
 
 export default mongoose.model("WikiEntry", wikiEntrySchema);
