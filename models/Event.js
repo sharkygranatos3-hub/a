e3b4885bffee4fa82f4565a1d4225ea979d35435
sharkygranatos3-hub <sharkygranatos3@gmail.com>
2025-10-23 16:20:14 +0200

    Update Event.js

--- models/Event.js
+++ models/Event.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  start: { type: String, required: true },  // ISO-Format YYYY-MM-DD
+  start: { type: String, required: true },
   end: { type: String, required: true },
   type: { type: String, enum: ["public", "private", "group"], default: "public" },
-  group: { type: String, default: "" },
-  desc: { type: String, default: "" },
-  owner: { type: String, required: true }, // Benutzer-ID oder Name
+  group: { type: String },
+  desc: { type: String },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   ownerName: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
+  ownerRank: { type: String, required: true },
+}, { timestamps: true });
 
 export default mongoose.model("Event", eventSchema);
